b737c72fff4ec8b4961c4bdcaeeb705ab74aa7bc
brc <brcooley@cs.wm.edu>
2012-01-13 23:45:47 -0500

    Cleaned up general code, added ASCII to Pinyin converter

--- chineseRandomCard.py
+++ chineseRandomCard.py
@@ -10,15 +10,11 @@ import sys
 import os.path
 import random
 import json
+import re
 from optparse import *
 
-PROG_TITLE = 'ChineseFlashcards'
+PROG_TITLE = 'CLIFlashcards'
 VERSION = '0.3'
-
-'''	Start 	= [i v]'
-	Mid		= [a e h i n o u v]
-	End 	= [a e g i n o r u v]
-'''
 
 PINYIN_TRANSLATIONS = {
 	'ü': 'v',
@@ -27,10 +23,6 @@ PINYIN_TRANSLATIONS = {
 	'ǎ': 'a3', 'ě': 'e3', 'ǐ': 'i3', 'ǒ': 'o3', 'ǔ': 'u3', 'ǚ': 'v3',
 	'à': 'a4', 'è': 'e4', 'ì': 'i4', 'ò': 'o4', 'ù': 'u4', 'ǜ': 'v4'
 }
-
-''' DICT = { 	tone0 = {},
-			tone1 = {} }
-'''
 
 
 def main():
@@ -47,38 +39,36 @@ def main():
 	wrongAns = []
 
 #	胡琴弹奏
-# 	āáǎà
-# 	ēèěé
-# 	īǐíì
-# 	ōóǒò
-# 	ūúǔù
-# 	üǖǘǚǜ
 
 	if args.interact:
 		while True:
 			cmd = input('-=> ').lower()
+			print(cmd)
 			if cmd == 'q':
 				sys.exit(0)
-			elif cmd == 't':
+			elif cmd == 'p2a':
 				toTrans = input('? ')
 				print(pinyinToASCII(toTrans))
+			elif cmd == 'a2p':
+				toTrans = input('? ')
+				print(asciiToPinyin(toTrans, False))
 
 	if args.build:
 		createVocab(args.build)
 	if args.to_raw:
 		toRaw(args.to_raw)
 
+	# Builds list of word data from each file passed in
+	wordList = []
 	for cFile in cardfiles:
-		cardIn = open(cFile,'r')
-		for char in cardIn.readlines():
-			word, answer = char.split()
-			for ans in answer.split(';'):
-				ansList.append(ans.strip().lower())
-			wordList.append([word.strip().lower(),ansList])
-			ansList = []
-		cardIn.close()
+		with open(cFile) as wordsIn:
+			wordList += json.load(wordsIn)
 
 	random.shuffle(wordList)
+	print('{} : {}'.format(len(wordList), wordList))
+	sys.exit()
+
+	# for wordData in wordList:
 
 	for word in wordList:
 		possAns = input('{}? '.format(word[0]))
@@ -99,22 +89,19 @@ def main():
 
 
 def createVocab(filename):
-	'''Convert raw vocab input into a JSON list.'''
+	'''Convert vraw vocab input into a JSON list.'''
 	with open(filename,'r') as fIn:
 		wordData = fIn.read().split('----')
 		words = wordData[0].split()
 		alts = wordData[1].split('  ')
-		# meanings = wordData[2].split()
+		meanings = wordData[2].split('  ')
+		pos = wordData[3].split('  ')
 
-		if len(words) != len(alts):  # != len(meanings):
-			print('Error converting, unequal number of word/alt pairings.')
+		if len(words) != len(alts) != len(meanings):
+			print('Error converting, unequal number of word/alt/meaning pairings.')
 			sys.exit(1)
-		wordJSON = [{'word':x, 'alt':[''.join(y.split()).strip(), pinyinToASCII(y.strip())], 'pos':[], 'meaning':[]} for x,y in zip(words, alts)]
-		# for entry in wordJSON:
-			# entry['alt'].append(pinyinToASCII(entry['alt'][0]))
-		# for i, entry in enumerate(wordJSON):
-			# for meaning in meanings[i]:
-				# entry['meaning'].append(meaning)
+		wordJSON = [{'word':w, 'alt':[''.join(x.split()).strip(), pinyinToASCII(x.strip())], 'pos':y.strip().split(', '),
+					'meaning':z.strip().split(', ')} for w,x,y,z in zip(words, alts, pos, meanings)]
 
 	if (os.path.isfile(filename.split('.')[0] + '.vocab')):
 		if ('y' != input('Warning, .vocab file already exists, overwrite? ')[0].lower()):
@@ -136,22 +123,43 @@ def pinyinToASCII(phrase):
 		neutralTone = True
 		for ch in word:
 			if ch in PINYIN_TRANSLATIONS.keys():
-				toReturn += (word[:word.index(ch)] + PINYIN_TRANSLATIONS[ch][0] + word[word.index(ch)+1:] + PINYIN_TRANSLATIONS[ch][1])
+				toReturn += (word[:word.index(ch)] + PINYIN_TRANSLATIONS[ch][0] +
+					word[word.index(ch)+1:] + PINYIN_TRANSLATIONS[ch][1])
 				neutralTone = False
 				break
 		if neutralTone == True:
 			toReturn += word
-			
 	return toReturn
 
 
+def asciiToPinyin(phrase, preserveSpaces=True):
+	'''Inverse of pinyinToASCII, converts words from word# into pinyin.'''
+	aWords = phrase.split(' ')
+	toReturn = ''
+
+	for word in aWords:
+		# print('Working on: {}'.format(word))
+		if word[-1:] in ['1','2','3','4']:
+			vowels = [x.lower() for x in re.findall(r'[a|e|i|o|u|v]*', word, re.IGNORECASE) if len(x) > 0][0]
+			vowel = sorted(vowels)[0] if vowels[0] != 'i' or len(vowels) == 1 else vowels[1]
+			ASCII_TRANSLATIONS = {v:k for k, v in PINYIN_TRANSLATIONS.items()}
+			toReturn += word[:word.index(vowel)] + ASCII_TRANSLATIONS[vowel + word[-1:]] + word[word.index(vowel)+1:-1]
+		elif 'v' in word:
+			toReturn += word[:word.index('v')] + 'ü' + word[word.index('v')+1:]
+		else:
+			toReturn += phrase
+		if preserveSpaces:
+			toReturn += ' '
+	return toReturn.strip()
+
+
 def toRaw(filename):
-	'''Converts old ctxt format to raw format.'''
+	'''Converts old ctxt format to vraw.inc format.'''
 	with open(filename, 'r') as fIn:
 		data = [(line.split()[0], line.split()[1]) for line in fIn.readlines()]
 	wordStr = '\n'.join([x[0] for x in data])
-	altStr = ' '.join([x[1] for x in data])
-	with open(filename.split('.')[0] + '.vraw', 'w') as fOut:
+	altStr = '  '.join([asciiToPinyin(' '.join(re.findall(r'[A-z]+[1-4]?',x))) for _, x in data])
+	with open(filename.split('.')[0] + '.vraw.inc', 'w') as fOut:
 		print('{}\n----\n{}'.format(wordStr, altStr), file=fOut)
 	sys.exit(0)
 
